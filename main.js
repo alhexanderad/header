610d1fd1f9d53b34de1d24be36ffa89bd554a752
alhexanderad <alhexanderad@gmail.com>
2022-01-31 23:08:07 -0500

    la codificacion de smooth y sticky

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-console.log("ñskdkñasmdfñkms");
+/**Codigo de header */
 window.addEventListener('scroll', (event) =>{
-  console.log(event);
+  const header = document.querySelector('header');
+  console.log(window.scrollY);
+  header.classList.toggle('sticky',window.scrollY > 0)
+});
+
+/**Links */
+document.querySelectorAll('.link').forEach(link =>{
+  link.addEventListener('click', (e)=>{
+    e.preventDefault();
+    
+    /*console.log(link.href.split('#')[1]);*/
+    const hrefHash = link.href.split('#')[1];
+    const element = document.getElementById(hrefHash)
+    element.scrollIntoView({
+      block:'start',
+      behavior: 'smooth'
+    })
+
+  })
 })
+
+
