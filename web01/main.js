0b3b73d79130162d74af228a51c9f5144a6bf388
Alex Hector Astuhuaman Delgado <alhexanderad@gmail.com>
2022-04-16 13:11:19 -0500

    Se comÂpleto la creacion de layout page

--- web01/main.js
+++ web01/main.js
@@ -1,4 +1,17 @@
+/**codigo para menu */
+const navBar = document.querySelector('.header .navbar')
+const menuButton = document.querySelector('.header .menu')
+
+menuButton.addEventListener('click', () =>{
+  navBar.classList.toggle('show');
+  menuButton.classList.toggle('fa-times');
+})
+
+/**Codigo para scroll */
 document.onscroll=()=>{
+  navBar.classList.remove('show');
+  menuButton.classList.remove('fa-times');
+
   if(window.scrollY>0){
     document.querySelector('.header').classList.add('active');
   }else{
